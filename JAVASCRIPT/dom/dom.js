81248786ed316fffda4975afce137789ca809fc7
Camila <116912821+alvescamila87@users.noreply.github.com>
2024-01-23 15:11:52 -0300

    Dom

---
+++ JAVASCRIPT/dom/dom.js
@@ -0,0 +1,1 @@
+document.querySelector("#txtrect").innerHTML = "Teste;"
---
+++ JAVASCRIPT/funcao/funcao2.js
@@ -0,0 +1,6 @@
+function multiplicaValores(num1, num2) {
+    var resultado = num1 * num2;
+    return resultado;
+}
+
+console.log(multiplicaValores(10, 88));
